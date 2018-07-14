4d60aba5b07df9b883b0aeff733035b2e5da7820
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2018-07-13 22:30:58 -0400

    Added some new files for IO.

---
+++ src/FileIO.h
@@ -0,0 +1,21 @@
+/// ----------------------------------------------------------------------------
+/// File     : FileIO.h
+/// Info     :
+/// Author   : Justo Martin-Albo
+/// Creation : 9 July 2018
+/// ----------------------------------------------------------------------------
+
+#ifndef FILE_IO
+#define FILE_IO
+
+
+class FileIO
+{
+public:
+  // Constructor
+  FileIO();
+  // Destructor
+  ~FileIO();
+};
+
+#endif
---
+++ src/TestFileIO.cpp
@@ -0,0 +1,18 @@
+/// ----------------------------------------------------------------------------
+/// File     : TestFileIO.cpp
+/// Author   : Justo Martin-Albo
+/// Creation : 9 July 2018
+/// ----------------------------------------------------------------------------
+
+#include "FileIO.h"
+
+#include <iostream>
+
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "argc: " << argc    << std::endl;
+  std::cout << "argv: " << argv[1] << std::endl;
+  /* code */
+  return 0;
+}
