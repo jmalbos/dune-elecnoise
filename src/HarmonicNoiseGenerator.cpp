575d76b182f4a9e874689e73926a3ff20228c840
Miquel <miquel.nebot@ed.ac.uk>
2018-08-10 14:40:58 +0100

    Complilable version

--- src/HarmonicNoiseGenerator.cpp
+++ src/HarmonicNoiseGenerator.cpp
@@ -20,28 +20,29 @@ void HarmonicNoiseGenerator::Generate(std::vector< std::vector<double> >& sample
 std::vector<double> v_noise;
 
 	//for (int i = 0; i < samples.size(); i++){ //channel loop//
-	for (std::vector<int>::iterator it = samples.begin() ; it != samples.end(); ++it)
+	for (std::vector<std::vector<double>>::iterator it = samples.begin() ; it != samples.end(); ++it)
+		
 	{
-		 if (it<1600) 	//UChannels [0,799] + VChannels [800,1599] 40 channesl per FEMB//
+		 if (std::distance(samples.begin(), it) <1600) 	//UChannels [0,799] + VChannels [800,1599] 40 channesl per FEMB//
 	 	{
-	 		if (it % 40 ==0)
+	 		if (std::distance(samples.begin(), it) % 40 ==0)
 	 		{
 		 	v_noise = Sinusoidal(a,phi);
 	 		}
 	 		
 	 		for (int i = 0; i < 4492; i++)
-	 		samples[it][i]=	 samples[it][i] + v_noise[i];
+	 		samples[std::distance(samples.begin(), it)][i]=	 samples[std::distance(samples.begin(), it)][i] + v_noise[i];
 	 	
 	 	}
 	 	else 			//XChannels [1600,2560] 48 channesl per FEMB//
 	 	{
-			if (it-1600 % 48 ==0)
+			if (std::distance(samples.begin(), it)-1600 % 48 ==0)
 	 		{
 		 	v_noise = Sinusoidal(a,phi);
 	 		}
 	 		
 	 		for (int i = 0; i < 4492; i++)
-	 		samples[it][i]=	 samples[it][i] + v_noise[i];
+	 		samples[std::distance(samples.begin(), it)][i]=	 samples[std::distance(samples.begin(), it)][i] + v_noise[i];
 
 	 	}
 
@@ -57,7 +58,7 @@ std::vector<double> HarmonicNoiseGenerator::Sinusoidal(double a, double phi)
     //v.reserve(samples[0].size());
     v.reserve(4492);
     double x,y;
-    for (std::vector<int>::iterator it = v.begin() ; it != v.end(); ++it){
+    for (std::vector<double>::iterator it = v.begin() ; it != v.end(); ++it){
 		x = rand() % 4492; //4492 == vector.size(); // ADC number of samples or number of time ticks //
 		y = a * sin(x + phi);
 		v.push_back(y);
